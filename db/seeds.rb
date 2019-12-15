53a7c7422b478cda9b8f4a5b167a8f55585a5daa
austin_smith <trunk9slamchest@gmail.com>
2019-12-15 00:14:41 -0500

    updated timestamp on traffics serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,42 +13,44 @@ months = [
 		"November",
 		"December"
 	]
-
-	User.create(
-		user_name: "admin1",
-		password: "123",
-		email: "admin@admin.com",
-		access: "admin",
-		first_name: "admin",
-		last_name: "admin",
-		gender: "Attack Helicopter",
-		birth_day: 21,
-		birth_month: "Augtober",
-		birth_year: 2001,
-		house_number: 8888,
-		street_name: "Admin Street",
-		city_town: "Admin Town",
-		state: "New York",
-		zip_code: 69420
-	)
-
-	10.times {
-		User.create(
-			user_name: "user#{numbers.sample}",
-			password: "pw",
-			email: Faker::Internet.free_email,
-			first_name: Faker::Name.first_name,
-			last_name: Faker::Name.last_name,
-			gender: Faker::Gender.binary_type,
-			birth_day: rand(1..28),
-			birth_month: "#{months.sample}",
-			birth_year: rand(1950..2005),
-			house_number: rand(1..999),
-			street_name: "#{Faker::Address.street_name} #{Faker::Address.street_suffix}",
-			city_town: Faker::Address.community,
-			state: Faker::Address.state,
-			zip_code: rand(10000..99999)
-		)
-	}
 	
-Traffic.create(user_id: 1, interaction: "click", element: "test_button3")
+Traffic.destroy_all
+
+# 	User.create(
+# 		user_name: "admin1",
+# 		password: "123",
+# 		email: "admin@admin.com",
+# 		access: "admin",
+# 		first_name: "admin",
+# 		last_name: "admin",
+# 		gender: "Attack Helicopter",
+# 		birth_day: 21,
+# 		birth_month: "Augtober",
+# 		birth_year: 2001,
+# 		house_number: 8888,
+# 		street_name: "Admin Street",
+# 		city_town: "Admin Town",
+# 		state: "New York",
+# 		zip_code: 69420
+# 	)
+
+# 	10.times {
+# 		User.create(
+# 			user_name: "user#{numbers.sample}",
+# 			password: "pw",
+# 			email: Faker::Internet.free_email,
+# 			first_name: Faker::Name.first_name,
+# 			last_name: Faker::Name.last_name,
+# 			gender: Faker::Gender.binary_type,
+# 			birth_day: rand(1..28),
+# 			birth_month: "#{months.sample}",
+# 			birth_year: rand(1950..2005),
+# 			house_number: rand(1..999),
+# 			street_name: "#{Faker::Address.street_name} #{Faker::Address.street_suffix}",
+# 			city_town: Faker::Address.community,
+# 			state: Faker::Address.state,
+# 			zip_code: rand(10000..99999)
+# 		)
+# 	}
+	
+# Traffic.create(user_id: 1, interaction: "click", element: "test_button3")
